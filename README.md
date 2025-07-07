4e71584dd2c09b5e9b651f4a967dc5269e8055f7
JoshCodesPhysics <joshhorswill@gmail.com>
2025-07-07 22:08:11 +0100

    feat: torch-based GPU operations

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Repo for messing around with performance boosts in Python using a mock transformer inference pass
+
+Do `conda env create -f environment. yml`
+This code uses MPS rather than NVIDIA drivers, but should be straightforward to change.
--- layer_norm.py
+++ layer_norm.py
@@ -1,10 +1,12 @@
-import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.mps.profiler as profiler
 
 # import line_profiler as lp
 # import memory_profiler as mp
 
 
-@profile
+
 def embed_tokens(token_ids, token_embedding, positional_embedding):
     """Embed tokens using token and positional embeddings.
     Args:
@@ -14,11 +16,10 @@ def embed_tokens(token_ids, token_embedding, positional_embedding):
     Returns:
         Embedded tensor of shape (batch_size, sequence_length, num_hidden_dims).
     """
-    embedded_tokens = token_embedding[token_ids]
-    return embedded_tokens + positional_embedding[np.newaxis, :, :]
+    return token_embedding[token_ids] + positional_embedding.unsqueeze(0)
 
 
-@profile
+
 def layer_norm(input_tensor, gamma_weights, beta_weights, eps=1e-5):
     """Layer normalization function.
     Args:
@@ -29,14 +30,16 @@ def layer_norm(input_tensor, gamma_weights, beta_weights, eps=1e-5):
     Returns:
         Normalized tensor of the same shape as x.
     """
-    mean = np.mean(input_tensor, axis=-1, keepdims=True)
-    var = np.var(input_tensor, axis=-1, keepdims=True)
-    # Eps prevents division by zero
-    norm = (input_tensor - mean) / np.sqrt(var + eps)
-    return norm * gamma_weights + beta_weights
+    return F.layer_norm(
+        input_tensor,
+        normalized_shape=input_tensor.shape[-1:],
+        weight=gamma_weights,
+        bias=beta_weights,
+        eps=eps
+    )
 
 
-@profile
+
 def relu(input_tensor):
     """Rectified Linear Unit activation function.
     Args:
@@ -44,10 +47,10 @@ def relu(input_tensor):
     Returns:
         Output tensor with ReLU applied.
     """
-    return np.maximum(0, input_tensor)
+    return F.relu(input_tensor, inplace=True)
 
 
-@profile
+
 def softmax(input_tensor, axis=-1):
     """Softmax function.
     Args:
@@ -56,14 +59,10 @@ def softmax(input_tensor, axis=-1):
     Returns:
         Output tensor with softmax applied.
     """
-    input_tensor = input_tensor - np.max(
-        input_tensor, axis=axis, keepdims=True
-    )  # prevent overflow
-    exp = np.exp(input_tensor)
-    return exp / np.sum(exp, axis=axis, keepdims=True)
+    return F.softmax(input_tensor, dim=axis)
 
 
-@profile
+
 def reshape_for_heads(input_tensor, batch_size, sequence_length, heads, dims_per_head):
     """Reshape input tensor for multi-head attention.
     Args:
@@ -75,16 +74,12 @@ def reshape_for_heads(input_tensor, batch_size, sequence_length, heads, dims_per
     Returns:
         Reshaped tensor of shape (batch_size, heads, sequence_length, dims_per_head).
     """
-    if input_tensor.ndim != 3:
-        raise ValueError(
-            f"Expected input tensor to have 3 dimensions, got {input_tensor.ndim} dimensions."
-        )
-    return input_tensor.reshape(
+    return torch.reshape(input_tensor, (
         batch_size, sequence_length, heads, dims_per_head
-    ).transpose(0, 2, 1, 3)
+    )).permute(0, 2, 1, 3)
 
 
-@profile
+
 def causal_mask(size):
     """Create a causal mask for self-attention.
     Args:
@@ -92,26 +87,10 @@ def causal_mask(size):
     Returns:
         Causal mask of shape (size, size) with lower triangular part set to True.
     """
-    return np.tril(np.ones((size, size), dtype=bool))
+    return torch.tril(torch.ones((size, size), dtype=bool))
 
 
-def dropout(input_tensor, dropout_rate):
-    """Apply dropout to input tensor.
-    Args:
-        input_tensor: Input tensor of shape (batch_size, sequence_length, num_hidden_dims).
-        dropout_rate: Dropout rate (0.0 to 1.0).
-    Returns:
-        Output tensor with dropout applied.
-    """
-    if dropout_rate <= 0.0:
-        return input_tensor
-    mask = np.random.binomial(1, 1 - dropout_rate, size=input_tensor.shape).astype(
-        input_tensor.dtype
-    )
-    return input_tensor * mask / (1 - dropout_rate)
 
-
-@profile
 def self_attention_block(
     input_tensor,
     query_weights,
@@ -147,26 +126,26 @@ def self_attention_block(
     key = reshape_for_heads(key, batch_size, sequence_length, heads, dims_per_head)
     value = reshape_for_heads(value, batch_size, sequence_length, heads, dims_per_head)
     # query / key / value shape (batch_size, sequence_length, heads, dims_per_head)
-    # key.transpose(0, 1, 3, 2) shape (batch_size, sequence_length, dims_per_head, heads)
+    # key.permute(0, 1, 3, 2) shape (batch_size, sequence_length, dims_per_head, heads)
     # scores shape (batch_size, num_heads, sequence_length, sequence_length)
-    # Hence key.transpose(0, 1, 3, 2) is used to align dimensions for matrix multiplication.
-    scores = query @ key.transpose(0, 1, 3, 2) / np.sqrt(num_hidden_dims)
+    # Hence key.permute(0, 1, 3, 2) is used to align dimensions for matrix multiplication.
+    scores = torch.divide(query @ torch.permute(key, (0, 1, 3, 2)), (num_hidden_dims ** 0.5))
     # Lower triangular mask to prevent attending to future tokens
     # The token at the ith position can only attend to tokens from 0 to i,
     # since the ith row in the mask contains i True values
     # [:, None, :, :] reshapes the mask to align with the scores tensor, extends in the batch_size dimension
-    scores = np.where(mask[:, None, :, :], scores, -1e9)
+    scores = torch.where(mask[:, None, :, :], scores, -1e9)
 
     weights = softmax(scores, axis=-1)
 
     attended = weights @ value
-    attended = attended.transpose(0, 2, 1, 3).reshape(
+    attended = torch.permute(attended, (0, 2, 1, 3)).reshape(
         batch_size, sequence_length, num_hidden_dims
     )
     return attended
 
 
-@profile
+
 def mlp_block(input_tensor, mlp_weights1, mlp_bias1, mlp_weights2, mlp_bias2):
     """Feedforward MLP block.
     Args:
@@ -182,7 +161,7 @@ def mlp_block(input_tensor, mlp_weights1, mlp_bias1, mlp_weights2, mlp_bias2):
     return hidden @ mlp_weights2.T + mlp_bias2
 
 
-@profile
+
 def final_logits(output_tensor, head_weights, gamma_weights, beta_weights):
     """Project output to vocabulary logits after layer norm.
     output_tensor: (batch, seq, hidden)
@@ -216,8 +195,7 @@ def full_block(
     sequence_length,
     heads,
     dims_per_head,
-    mask,
-    dropout_rate,
+    mask
 ):
     """Full transformer block with embedding, self-attention, MLP, and final logits.
     Args:
@@ -266,7 +244,6 @@ def full_block(
     )
 
     attention_projection = attention_output @ projection_weights + projection_bias
-    attention_projection = dropout(attention_projection, dropout_rate)
     input_residual = layer_normed_input_tensor + attention_projection
 
     second_layer_normed_input_tensor = layer_norm(
@@ -280,7 +257,6 @@ def full_block(
         mlp_weights2,
         mlp_bias2,
     )
-    mlp_output = dropout(mlp_output, dropout_rate)
 
     second_input_residual = input_residual + mlp_output
 
@@ -291,6 +267,7 @@ def full_block(
 
 
 if __name__ == "__main__":
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
     print("Starting transformer pass...")
     # Dimensions
     batch_size = 32
@@ -302,90 +279,85 @@ if __name__ == "__main__":
 
     vocab_size = 50257  # typical GPT-style vocab
 
-    dropout_rate = 0.1  # typical dropout rate in transformers
 
     # The mean (0) and std dev (0.02) match the initialisation range in
     # standard transformer implementations (GPT, BERT, T5)
     # — this range prevents exploding activations and helps training stability.
 
     # Each token ID and token position maps to a vector in the token embedding matrix.
-    token_ids = np.random.randint(0, vocab_size, size=(batch_size, sequence_length))
-    token_embedding = np.random.normal(
+    token_ids = torch.randint(low=0, high=vocab_size, size=(batch_size, sequence_length), dtype = torch.long).to(device)
+    token_embedding = torch.normal(
         0, 0.02, size=(vocab_size, num_hidden_dims)
-    ).astype(np.float32)
-    positional_embedding = np.random.normal(
+    ).to(device)
+    positional_embedding = torch.normal(
         0, 0.02, size=(sequence_length, num_hidden_dims)
-    ).astype(np.float32)
-
-    # Input tensor: standard normal distribution (typical in transformers)
-    input_tensor = np.random.randn(batch_size, sequence_length, num_hidden_dims).astype(
-        np.float32
-    )
+    ).to(device)
 
     # Learned scale (gamma) and shift (beta) for each feature
-    gamma_weights1 = np.random.normal(
-        loc=1.0, scale=0.02, size=(num_hidden_dims,)
-    ).astype(np.float32)
-    beta_weights1 = np.random.normal(
-        loc=0.0, scale=0.02, size=(num_hidden_dims,)
-    ).astype(np.float32)
-    gamma_weights2 = np.random.normal(
-        loc=1.0, scale=0.02, size=(num_hidden_dims,)
-    ).astype(np.float32)
-    beta_weights2 = np.random.normal(
-        loc=0.0, scale=0.02, size=(num_hidden_dims,)
-    ).astype(np.float32)
-    gamma_weights3 = np.random.normal(
-        loc=1.0, scale=0.02, size=(num_hidden_dims,)
-    ).astype(np.float32)
-    beta_weights3 = np.random.normal(
-        loc=0.0, scale=0.02, size=(num_hidden_dims,)
-    ).astype(np.float32)
+    gamma_weights1 = torch.normal(
+        1.0, 0.02, size=(num_hidden_dims,)
+    ).to(device)
+    beta_weights1 = torch.normal(
+        0.0, 0.02, size=(num_hidden_dims,)
+    ).to(device)
+    gamma_weights2 = torch.normal(
+        1.0, 0.02, size=(num_hidden_dims,)
+    ).to(device)
+    beta_weights2 = torch.normal(
+        0.0, 0.02, size=(num_hidden_dims,)
+    ).to(device)
+    gamma_weights3 = torch.normal(
+        1.0, 0.02, size=(num_hidden_dims,)
+    ).to(device)
+    beta_weights3 = torch.normal(
+        0.0, 0.02, size=(num_hidden_dims,)
+    ).to(device)
 
     # Projection weights and bias
-    projection_weights = np.random.randn(num_hidden_dims, num_hidden_dims).astype(
-        np.float32
-    )
-    projection_bias = np.random.randn(num_hidden_dims).astype(np.float32)
+    projection_weights = torch.randn(num_hidden_dims, num_hidden_dims).to(device)
+    projection_bias = torch.randn(num_hidden_dims).to(device)
 
     # Attention weights
-    query_weights = np.random.randn(num_hidden_dims, num_hidden_dims).astype(np.float32)
-    key_weights = np.random.randn(num_hidden_dims, num_hidden_dims).astype(np.float32)
-    value_weights = np.random.randn(num_hidden_dims, num_hidden_dims).astype(np.float32)
+    query_weights = torch.randn(num_hidden_dims, num_hidden_dims).to(device)
+    key_weights = torch.randn(num_hidden_dims, num_hidden_dims).to(device)
+    value_weights = torch.randn(num_hidden_dims, num_hidden_dims).to(device)
 
     # MLP weights
-    mlp_weights1 = np.random.randn(dims_mlp, num_hidden_dims).astype(np.float32)
-    mlp_bias1 = np.random.randn(dims_mlp).astype(np.float32)
-    mlp_weights2 = np.random.randn(num_hidden_dims, dims_mlp).astype(np.float32)
-    mlp_bias2 = np.random.randn(num_hidden_dims).astype(np.float32)
+    mlp_weights1 = torch.randn(dims_mlp, num_hidden_dims).to(device)
+    mlp_bias1 = torch.randn(dims_mlp).to(device)
+    mlp_weights2 = torch.randn(num_hidden_dims, dims_mlp).to(device)
+    mlp_bias2 = torch.randn(num_hidden_dims).to(device)
 
-    mask = causal_mask(sequence_length)[None, :, :].repeat(batch_size, axis=0)
+    mask = causal_mask(sequence_length).unsqueeze(0).repeat(batch_size, 1, 1).to(device)
 
+    profiler.start()
+    torch.mps.synchronize()
     full_block_output = full_block(
-        token_ids,
-        token_embedding,
-        positional_embedding,
-        query_weights,
-        key_weights,
-        value_weights,
-        projection_weights,
-        projection_bias,
-        mlp_weights1,
-        mlp_bias1,
-        mlp_weights2,
-        mlp_bias2,
-        gamma_weights1,
-        beta_weights1,
-        gamma_weights2,
-        beta_weights2,
-        gamma_weights3,
-        beta_weights3,
-        batch_size,
-        sequence_length,
-        heads,
-        dims_per_head,
-        mask,
-        dropout_rate
+    token_ids,
+    token_embedding,
+    positional_embedding,
+    query_weights,
+    key_weights,
+    value_weights,
+    projection_weights,
+    projection_bias,
+    mlp_weights1,
+    mlp_bias1,
+    mlp_weights2,
+    mlp_bias2,
+    gamma_weights1,
+    beta_weights1,
+    gamma_weights2,
+    beta_weights2,
+    gamma_weights3,
+    beta_weights3,
+    batch_size,
+    sequence_length,
+    heads,
+    dims_per_head,
+    mask
     )
+    torch.mps.synchronize()
+    profiler.stop()
 
     print("Tranformer pass completed")
